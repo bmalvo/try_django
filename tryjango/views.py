0a6ad902e014a11a12dc01ee811f3cb8194b6941
patryk_s <boydmalvo@gmail.com>
2022-04-26 20:11:28 +0200

    rendering data from database

--- tryjango/views.py
+++ tryjango/views.py
@@ -4,7 +4,7 @@ To render HTML web pages
 
 from django.http import HttpResponse
 import random
-
+from articles.models import Articles
 
 
 def home_view(request):
@@ -14,13 +14,14 @@ def home_view(request):
     """
 
     name = 'Boyd'
-    number = random.randint(0,100)
+    random_id = random.randint(1, 4)
+    article_obj = Articles.objects.get(id=random_id)
 
     H1_STRING = f"""
-    <h1> Hello {name}! </h1>"""
+    <h1> {article_obj.title} (id: {article_obj.id}) ! </h1>"""
 
     P_STRING = f"""
-    <p> Today lucky number is: '{number}'</p>
+    <p> {article_obj.content}</p>
     """
     HTML_STRING = H1_STRING + P_STRING
 
