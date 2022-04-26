6f4eadf5fa86dd7a011b0ec8beb25bd3b59a506a
patryk_s <boydmalvo@gmail.com>
2022-04-26 17:38:27 +0200

    add random feature to main view

--- tryjango/views.py
+++ tryjango/views.py
@@ -3,11 +3,8 @@ To render HTML web pages
 """
 
 from django.http import HttpResponse
+import random
 
-
-HTML_STRING = """
-<h1> Hello World! </h1>
-"""
 
 
 def home_view(request):
@@ -15,5 +12,16 @@ def home_view(request):
     Take in a request (Django sends request)
     Return a HTML as a response (We pick to return a response)
     """
-    print('Stefka piękny kot')
+
+    name = 'Boyd'
+    number = random.randint(0,100)
+
+    H1_STRING = f"""
+    <h1> Hello {name}! </h1>"""
+
+    P_STRING = f"""
+    <p> Today lucky number is: '{number}'</p>
+    """
+    HTML_STRING = H1_STRING + P_STRING
+
     return HttpResponse(HTML_STRING)
