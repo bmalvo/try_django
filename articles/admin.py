cc2507db5c404f3b18a483b70e180cb064597423
patryk_s <boydmalvo@gmail.com>
2022-05-03 21:31:55 +0200

    add superuser and register model in django

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,13 @@
+from encodings import search_function
 from django.contrib import admin
 
 # Register your models here.
+from .models import Articles
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    search_fields = ['title', 'content']
+
+
+admin.site.register(Articles, ArticleAdmin)
